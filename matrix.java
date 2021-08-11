db784cb3c7c84aef335610f8250bbec8ef625ee9
ATHIRA SIVADASAN <75289141+athirasivadasan1999@users.noreply.github.com>
2021-08-11 09:04:53 +0530

    Update matrix.java

--- matrix.java
+++ matrix.java
@@ -1,11 +1,13 @@
 import java.util.*;
 
-public class matrix {
+public class matrix 
+{
     int row;
     int column;
     int[][] array = new int[10][10];
 
-    public void get_metrix(){
+    public void get_metrix()
+    {
         int rc,cc;
         Scanner sc= new Scanner(System.in);
         System.out.print("Enter size of matrix , row count : ");
@@ -13,22 +15,28 @@ public class matrix {
         System.out.print("Enter size of matrix , column count  : "); 
         this.column = sc.nextInt();
         System.out.print("Enter matrix elements : ");
-        for(rc=0;rc<this.row;rc++){
-            for(cc=0;cc<this.column;cc++){
+        for(rc=0;rc<this.row;rc++)
+        {
+            for(cc=0;cc<this.column;cc++)
+            {
                 this.array[rc][cc] = sc.nextInt();
             }
         }
     }
 
-    public static matrix sum(matrix c1, matrix c2) {
+    public static matrix sum(matrix c1, matrix c2) 
+    {
         int rc, cc;
         matrix temp = new matrix();
 
-        if (c1.row == c2.row && c1.column == c2.column) {
+        if (c1.row == c2.row && c1.column == c2.column) 
+        {
             temp.row =c1.row;
             temp.column = c1.column;
-            for (rc = 0; rc < c1.row; rc++) {
-                for (cc = 0; cc < c1.column; cc++) {
+            for (rc = 0; rc < c1.row; rc++) 
+            {
+                for (cc = 0; cc < c1.column; cc++) 
+                {
                     temp.array[rc][cc] =  c1.array[rc][cc] +  c2.array[rc][cc];
                 }
             }
@@ -40,16 +48,20 @@ public class matrix {
         return temp;
     }
 
-    public void display_matrix(){
+    public void display_matrix()
+    {
         int rc,cc;
-        for(rc=0;rc<this.row;rc++){
-            for(cc=0;cc<this.column;cc++){
+        for(rc=0;rc<this.row;rc++)
+        {
+            for(cc=0;cc<this.column;cc++)
+            {
                 System.out.print(this.array[rc][cc] );
             }
             System.out.println("");
         }
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         matrix first = new matrix();
         matrix second = new matrix();
         matrix temp = sum(first, second);
